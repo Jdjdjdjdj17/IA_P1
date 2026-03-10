0239384f7afbfd0ad8fd0a8fdbb8b9701f3d3bf7
Jdjdjdjdjd <jupasandoval17@gmail.com>
2026-03-10 13:45:34 -0600

    Create 2.1_Seccion1.py

---
+++ 2.1_Seccion1.py
@@ -0,0 +1,17 @@
+// 2.1.5: Trabajando con la funcion print()
+print("¡Hola, mundo!")
+
+// 2.1.12: La funcion print() y sus argumentos
+print(" Programming","Essencials","in",sep="***",end="...")
+print("Python")
+
+// 2.1.13: Dando formato a la salida
+print("     *\n","   * *\n","  *   *\n"," *     *\n","***   ***\n","  *   *\n","  *   *\n","  *****")
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
