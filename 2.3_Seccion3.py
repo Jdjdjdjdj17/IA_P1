ab6667a7775282e869dc562e9b28da6d8131aee6
Jdjdjdjdjd <jupasandoval17@gmail.com>
2026-03-11 16:23:21 -0600

    2.3_Seccion3
    Se agrega la seccion 3 terminada

---
+++ 2.3_Seccion3.py
@@ -0,0 +1,14 @@
+print(-2**3,"\n")
+
+# 2.3.5: Cuestionario de seccion
+print("Pregunta 1: ¿Cuál es la salida del siguiente fragmento de código?")
+print('print((2 ** 4), (2 * 4.), (2 * 4)) ')
+print(16, 8.0, 4,"\n")
+
+print("Pregunta 2: ¿Cuál es la salida del siguiente fragmento de código?")
+print('print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))')
+print(-0.5, 0.5, 0, -1,"\n")
+
+print("Pregunta 3: ¿Cuál es la salida del siguiente fragmento de código?")
+print('print((2 % -4), (2 % 4), (2 ** 3 ** 2))')
+print(-2, 2, 512)
