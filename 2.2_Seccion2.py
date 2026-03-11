3b866ab8d5c1513f4a4b4d0b0e116d0dabf0cc7a
Jdjdjdjdjd <jupasandoval17@gmail.com>
2026-03-11 02:13:53 -0600

    2.2_Seccion2
    Los labs de la Seccion2

---
+++ 2.2_Seccion2.py
@@ -0,0 +1,16 @@
+# 2.2.6: Literales de Python
+print("Escriba un fragmento de código de una línea, utilizando la función print(), así como los caracteres de nuevalínea y de escape, para que coincida con el resultado esperado que se muestra en la salida.")
+print("\"Estoy\"\n\"\"aprendiendo\"\"\n\"\"\"Python\"\"\"\n")
+
+# 2.2.8: Cuestionario de seccion
+print("Pregunta 1: ¿Qué tipos de literales son los siguientes dos ejemplos?")
+print('"Hola ", "007"')
+print("Son cadenas\n")
+
+print("Pregunta 2: ¿Qué tipos de literales son los siguientes cuatro ejemplos?")
+print('"1.5", 2.0, 528, False')
+print("El prmero es cadena, el segundo es flotante, el tercero es entero y el cuarto es booleano\n")
+
+print("Pregunta 3: ¿Cuál es el valor decimal del siguiente número binario?")
+print(1011)
+print("Es 11 ;)\n")
