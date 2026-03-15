ede45cbf9f06929071d4c37ae7da7802f612b901
Jdjdjdjdjd <jupasandoval17@gmail.com>
2026-03-14 21:38:32 -0600

    Seccion 2.3

--- _2.3_Operadores_de_Python.py
+++ _2.3_Operadores_de_Python.py
@@ -1,8 +1,9 @@
 def practica_1():
+    print("=== 2.3.4: Potencia negativa ===\n")
     print(-2**3,"\n")
 
 def practica_2():
-    # 2.3.5: Cuestionario de seccion
+    print("=== 2.3.5: Cuestionario de seccion ===\n")
     print("Pregunta 1: ¿Cuál es la salida del siguiente fragmento de código?")
     print('print((2 ** 4), (2 * 4.), (2 * 4)) ')
     print(16, 8.0, 4,"\n")
@@ -17,8 +18,8 @@ def practica_2():
 
 while True:
     print("\n===== 2.3 Operadores de Python =====")
-    print("1. Potencia negativa")
-    print("2. Cuestionario de seccion")
+    print("1. 2.3.4 - Potencia negativa")
+    print("2. 2.3.5 - Cuestionario de seccion")
     print("0. Salir")
 
     opcion = input("\nElige una opcion: ")
