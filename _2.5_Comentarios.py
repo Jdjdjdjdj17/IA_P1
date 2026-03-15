f14cf6abedce80fcd3a95b9ae5a7794c801b305c
Jdjdjdjdjd <jupasandoval17@gmail.com>
2026-03-14 18:14:35 -0600

    Seccion 5

---
+++ _2.5_Comentarios.py
@@ -0,0 +1,11 @@
+# 2.5.3: Comentarios
+
+#este programa calcula los segundos en cierto número de horas determinadas
+
+a = 2 # número de horas
+seconds = 3600 # número de segundos en una hora
+
+print("Horas: ", a) #imprime el numero de horas
+print("Segundos en", a, "horas: ", a * seconds) # se imprime el numero de segundos en a horas
+
+#este el es fin del programa que calcula el numero de segundos en 2 horas
