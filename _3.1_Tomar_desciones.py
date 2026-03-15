83a70754babd9b895aa322b5f85eda678db8fd6a
Jdjdjdjdjd <jupasandoval17@gmail.com>
2026-03-15 13:51:26 -0600

    Seccion 3.1

--- _3.1_Tomar_desciones.py
+++ _3.1_Tomar_desciones.py
@@ -53,13 +53,41 @@ Nota: este país feliz nunca devuelve dinero a sus ciudadanos. Si el impuesto ca
 
     print("El impuesto es de: ", round(i), "\n")
 
+def practica_4():
+    print("=== 3.1.12: Fundamentos de la sentencia if-elif-else ===\n")
+    print("""Escenario
+
+Como seguramente sabrás, debido a algunas razones astronómicas, el año puede ser bisiesto o común. Los primeros tienen una duración de 366 días, mientras que los últimos tienen una duración de 365 días.
+
+Desde la introducción del calendario Gregoriano (en 1582), se utiliza la siguiente regla para determinar el tipo de año:
+
+    si el número del año no es divisible entre cuatro, es un año común.
+    de lo contrario, si el número del año no es divisible entre 100, es un año bisiesto.
+    de lo contrario, si el número del año no es divisible entre 400, es un año común.
+    de lo contrario, es un año bisiesto.
+
+Observa el código en el editor - solo lee un número de año y debe completarse con las instrucciones que implementan la prueba que acabamos de describir.
+
+El código debe mostrar uno de los dos mensajes posibles, que son Año Bisiesto o Año Común, según el valor ingresado.
+
+Sería bueno verificar si el año ingresado cae en la era Gregoriana y emitir una advertencia de lo contrario: No dentro del período del calendario Gregoriano. Consejo: utiliza los operadores != y %.""")
+
+    a = int(input("Introdusca un año: "))
+
+    if a < 1582 : print("Fuera del periodo del calendario Gregoriano")
+    elif a % 4 != 0 : print("Año comun")
+    elif a % 100 != 0 : print("Año bisiesto")
+    elif a % 400 != 0 : print("Año comun")
+    else : print("Año bisiesto")
+
 while True:
     print("\n===== 3.1 Tomar decisiones =====")
     print("1. 3.1.6  - Variables - Preguntas y respuestas")
     print("2. 3.1.10 - Operadores de comparacion y ejecucion condicional")
     print("3. 3.1.11 - Fundamentos de la sentencia if-else")
+    print("4. 3.1.12 - Fundamentos de la sentencia if-elif-else")
     print("0. Salir")
-
+                      
     opcion = input("\nElige una opcion: ")
 
     if opcion == "1":
@@ -68,6 +96,8 @@ while True:
         practica_2()
     elif opcion == "3":
         practica_3()
+    elif opcion == "4":
+        practica_4()
     elif opcion == "0":
         print("Hasta luego!")
         break
